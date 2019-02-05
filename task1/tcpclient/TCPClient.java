410d3e8ea6b5000bf692dec2700625fc7ac423eb
LinusBF <linus.webdevelopment@gmail.com>
2019-02-05 21:34:42 +0100

    Started and finished task 1

---
+++ task1/tcpclient/TCPClient.java
@@ -0,0 +1,57 @@
+package tcpclient;
+
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+
+    public static String askServer(String hostname, int port, String ToServer) throws IOException {
+        Socket socket = setupSocket(hostname, port);
+
+        if(ToServer != null) writeToServer(ToServer, socket);
+        String serverResponse = readServerResponse(socket);
+
+        tearDownSocket(socket);
+
+        return serverResponse;
+    }
+
+    public static String askServer(String hostname, int port) throws IOException {
+        return askServer(hostname, port, null);
+    }
+
+    private static Socket setupSocket(String hostname, int port) throws IOException {
+        Socket s = new Socket(hostname, port);
+        s.setSoTimeout(2000);
+        return s;
+    }
+
+    private static void writeToServer(String message, Socket s) throws IOException {
+        BufferedReader br = new BufferedReader(new StringReader(message));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        String currentLine = null;
+
+        while ((currentLine = br.readLine()) != null) {
+            out.writeBytes(currentLine + "\n");
+        }
+    }
+
+    private static String readServerResponse(Socket s) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String currentLine = null;
+        try {
+            while ((currentLine = br.readLine()) != null) {
+                sb.append(currentLine);
+            }
+        } catch (SocketTimeoutException e) {
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    private static void tearDownSocket(Socket s) throws IOException {
+        s.close();
+    }
+}
